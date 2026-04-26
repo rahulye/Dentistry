8be1f54494b6a518a4a6f0cdc8fe6f30652ce4c2
rahulye <rahulmykris@gmail.com>
2026-04-27 01:11:40 +0530

    chore: add peinstall script for prisma schema

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,8 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
-const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const connectionString = process.env.DATABASE_URL as string;
 
-export { prisma };
+const adapter = new PrismaNeon({ connectionString });
+
+export const prisma = new PrismaClient({ adapter });
